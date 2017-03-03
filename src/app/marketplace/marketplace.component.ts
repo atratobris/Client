d80df99e74ea4b579c93bac75c5e1a75fed008e3
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-03 22:32:30 +0000

    [marketplace] Render sketches for marketplace

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Sketch } from '../sketch/sketch';
+import { SketchService } from '../sketch/sketch.service';
+
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
   styleUrls: ['./marketplace.component.sass']
 })
 export class MarketplaceComponent implements OnInit {
+  public sketches: Sketch[];
 
-  constructor() { }
+  constructor(private sketchService: SketchService) {}
 
   ngOnInit() {
+    this.sketchService.marketplace().then( ( sketches: Sketch[] ) => {
+      this.sketches = sketches;
+    });
   }
 
 }
