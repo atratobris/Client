c112cb087dc9fcfe69b80c4fc7a7813ec940354b
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-04 18:40:54 +0000

    [auth] Add the user class and auth form

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-authentication',
@@ -7,14 +8,22 @@ import { AuthenticationService } from './authentication.service';
   styleUrls: ['./authentication.component.sass']
 })
 export class AuthenticationComponent implements OnInit {
+  public login = true;
+  public user: User;
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService) {
+    this.user = new User();
+  }
 
   ngOnInit() {
     // if the session is authenticated, send to dashboard
     if (this.authenticationService.loggedIn()) {
       this.authenticationService.handleLoggedIn();
     }
+  }
+
+  submit(): void {
+    console.log(this.user);
   }
 
 }
