85606ef5bd1d608884962dcf1057d652a431dbef
Paul Cristian <pp13003@my.bristol.ac.uk>
2017-03-20 01:16:33 +0000

    laptop input and output

---
+++ src/app/laptop/laptop-input/laptop-input.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Ng2Cable, Broadcaster } from 'ng2-cable/js/index';
+import { BrowserDetails } from '../../lib/browser-details';
+
+import { ENV } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-laptop-input',
+  templateUrl: './laptop-input.component.html',
+  styleUrls: ['./laptop-input.component.sass']
+})
+export class LaptopInputComponent implements OnInit, OnDestroy {
+  public events: string[] = [];
+  private url: string = ENV.apiWs;
+  private deviceType: string = 'Input';
+  private mac: string = `${localStorage.getItem('atrato-user-id')}|${BrowserDetails.getDetails()}${this.deviceType}`;
+
+  constructor(private ng2cable: Ng2Cable, private broadcaster: Broadcaster) {
+    this.events.push(`Registering laptop with mac: ${this.mac}`);
+    this.ng2cable.setCable(this.url);
+  }
+
+  ngOnInit() {
+    this.registerToChannel();
+  }
+
+  registerToChannel(): void {
+    this.ng2cable.subscription = this.ng2cable.cable.subscriptions.create({ channel: 'SketchChannel',
+                                      mac: this.mac, type: this.deviceType });
+
+    this.events.push(`Started connection with ${this.ng2cable.subscription.consumer.url}`);
+  }
+
+  triggerInput(): void {
+    this.ng2cable.subscription.perform("blink", { "mac": this.mac })
+  }
+
+  ngOnDestroy() {
+    this.ng2cable.unsubscribe();
+  }
+}
--- src/app/laptop/laptop.component.ts
+++ src/app/laptop/laptop.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Ng2Cable } from 'ng2-cable/js/index';
-import { BrowserDetails } from '../lib/browser-details';
-
-import { ENV } from '../../environments/environment';
 
 @Component({
   selector: 'app-laptop',
@@ -10,32 +6,26 @@ import { ENV } from '../../environments/environment';
   styleUrls: ['./laptop.component.sass']
 })
 export class LaptopComponent implements OnInit, OnDestroy {
-  public events: string[] = [];
-  private url: string = ENV.apiWs;
-  private mac: string = `${localStorage.getItem('atrato-user-id')}|${BrowserDetails.getDetails()}`;
+  private input: boolean;
+  private output: boolean;
 
-  constructor(private ng2cable: Ng2Cable) {
-    this.events.push(`Registering laptop with mac: ${this.mac}`);
-    this.ng2cable.setCable(this.url);
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.ng2cable.subscription = this.ng2cable.cable.subscriptions.create({ channel: 'SketchChannel', mac: this.mac, type: 'Screen' }, {
-      received: (data) => {
-        const message = data.message;
-        this.events.push(`Received channel message with type: ${message.type}`);
-        if (message.type === 'link_opener') {
-          this.events.push(`Going to open ${message.url}`);
-          window.open(message.url);
-        }
-      }
-    });
-
-    this.events.push(`Started connection with ${this.ng2cable.subscription.consumer.url}`);
   }
 
   ngOnDestroy() {
-    this.ng2cable.unsubscribe();
+  }
+
+  activateOutput(): void {
+    this.input = false;
+    this.output = true;
+
+  }
+
+  activateInput(): void {
+    this.output = false;
+    this.input = true;
   }
 
 }
